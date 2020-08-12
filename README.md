74dfbc921d7127225f7e7a353d8a91d88c83bd5e
Christer Edvartsen <cogo@starzinger.net>
2020-08-12 17:26:43 +0200

    Add abstract storage adapter test case, originally from imbo/imbo

--- README.md
+++ README.md
@@ -8,7 +8,57 @@
 
 ## Usage
 
-SDK for storage and database adapters used with Imbo. This package contains some abstract test cases **must** be used by all adapters for Imbo, making sure they all pass at least the common tests. Adapters should also add specific tests when needed.
+SDK for storage and database adapters used with Imbo. This package contains some abstract test cases that **must** be used by all adapters for Imbo, making sure they all pass at least the common tests. Adapters should also add specific tests when needed.
+
+### Main database adapters
+
+If you implement `Imbo\Database\DatabaseInterface`, you should include a test that looks something like this:
+
+```php
+<?php declare(strict_types=1);
+namespace Imbo\Database;
+
+class MyAdapterTest extends DatabaseTests {
+    protected function getAdapter() : MyAdapter {
+        return new MyAdapter(/* ... */);
+    }
+
+    public function setUp() : void {
+        // Remember to execute the parent setUp() method to initialize the adapter
+        parent::setUp();
+
+        // Perform database-specific cleanup
+        // ...
+    }
+}
+```
+
+This will make the adapter implementation run all relevant tests in the abstract test case.
+
+### Main storage adapters
+
+If you implement `Imbo\Storage\StorageInterface`, you should include a test that looks something like this:
+
+```php
+<?php declare(strict_types=1);
+namespace Imbo\Storage;
+
+class MyAdapterTest extends StorageTests {
+    protected function getAdapter() : MyAdapter {
+        return new MyAdapter(/* ... */);
+    }
+
+    public function setUp() : void {
+        // Remember to execute the parent setUp() method to initialize the adapter
+        parent::setUp();
+
+        // Perform database-specific cleanup
+        // ...
+    }
+}
+```
+
+This will make the adapter implementation run all relevant tests in the abstract test case.
 
 ## License
 
---
+++ src/Storage/StorageTests.php
@@ -0,0 +1,129 @@
+<?php declare(strict_types=1);
+namespace Imbo\Storage;
+
+use Imbo\Exception\StorageException;
+use PHPUnit\Framework\TestCase;
+
+/**
+ * Base test case for database adapters
+ */
+abstract class StorageTests extends TestCase {
+    protected StorageInterface $adapter;
+    protected string $user = 'user';
+    protected string $imageIdentifier = 'image identifier';
+    protected string $fixturesDir  = __DIR__ . '/../Fixtures';
+    protected string $imageData;
+
+    /**
+     * Get the adapter we want to test
+     *
+     * @return StorageInterface
+     */
+    abstract protected function getAdapter() : StorageInterface;
+
+    public function setUp() : void {
+        $this->imageData = (string) file_get_contents($this->fixturesDir . '/image.png');
+        $this->adapter   = $this->getAdapter();
+    }
+
+    /**
+     * @covers ::store
+     */
+    public function testStoreAndGetImage() : void {
+        $this->assertTrue(
+            $this->adapter->store($this->user, $this->imageIdentifier, $this->imageData),
+            'Could not store initial image',
+        );
+
+        $this->assertSame(
+            $this->imageData,
+            $this->adapter->getImage($this->user, $this->imageIdentifier),
+            'Image data is out of sync',
+        );
+    }
+
+    /**
+     * @covers ::store
+     * @covers ::delete
+     * @covers ::getImage
+     */
+    public function testStoreDeleteAndGetImage() : void {
+        $this->assertTrue(
+            $this->adapter->store($this->user, $this->imageIdentifier, $this->imageData),
+            'Could not store initial image',
+        );
+
+        $this->assertTrue(
+            $this->adapter->delete($this->user, $this->imageIdentifier),
+            'Could not delete image',
+        );
+
+        $this->expectExceptionObject(new StorageException('File not found', 404));
+        $this->adapter->getImage($this->user, $this->imageIdentifier);
+    }
+
+    /**
+     * @covers ::delete
+     */
+    public function testDeleteImageThatDoesNotExist() : void {
+        $this->expectExceptionObject(new StorageException('File not found', 404));
+        $this->adapter->delete($this->user, $this->imageIdentifier);
+    }
+
+    /**
+     * @covers ::getImage
+     */
+    public function testGetImageThatDoesNotExist() : void {
+        $this->expectExceptionObject(new StorageException('File not found', 404));
+        $this->adapter->getImage($this->user, $this->imageIdentifier);
+    }
+
+    /**
+     * @covers ::getLastModified
+     */
+    public function testGetLastModifiedOfImageThatDoesNotExist() : void {
+        $this->expectExceptionObject(new StorageException('File not found', 404));
+        $this->adapter->getLastModified($this->user, $this->imageIdentifier);
+    }
+
+    /**
+     * @covers ::store
+     * @covers ::getLastModified
+     */
+    public function testGetLastModified() : void {
+        $this->assertTrue(
+            $this->adapter->store($this->user, $this->imageIdentifier, $this->imageData),
+            'Could not store initial image',
+        );
+
+        $lastModified = $this->adapter->getLastModified($this->user, $this->imageIdentifier);
+
+        $this->assertEqualsWithDelta(
+            time(),
+            $lastModified->getTimestamp(),
+            1,
+            'Last modification should be around now',
+        );
+    }
+
+    /**
+     * @covers ::imageExists
+     * @covers ::store
+     */
+    public function testCanCheckIfImageAlreadyExists() : void {
+        $this->assertFalse(
+            $this->adapter->imageExists($this->user, $this->imageIdentifier),
+            'Image is not supposed to exist',
+        );
+
+        $this->assertTrue(
+            $this->adapter->store($this->user, $this->imageIdentifier, $this->imageData),
+            'Could not store image',
+        );
+
+        $this->assertTrue(
+            $this->adapter->imageExists($this->user, $this->imageIdentifier),
+            'Image does not exist',
+        );
+    }
+}
